6bdfb03153fb8f338315b9c2087a806873b81d2d
smkqp8 <stephen.knutter@gmail.com>
2016-12-07 19:36:19 -0700

    update css

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,18 +33,6 @@ const LoggedInHead = props => {
     return(
       <ul className="nav navbar-nav pull-xs-right">
         <li className='nav-item'>
-          <Link to="" className="nav-link">
-            <i className="fa fa-home" aria-hidden="true"></i> Home
-          </Link>
-        </li>
-
-        <li className='nav-item'>
-          <Link to="settings" className="nav-link">
-            <i className="fa fa-cog" aria-hidden="true"></i> Settings
-          </Link>
-        </li>
-
-        <li className='nav-item'>
           <Link to={`/${props.currentUser.slug}`} className="nav-link">
             <img src={props.currentUser.profile_pic === 'user-placeholder.png' ? `/images/small-${props.currentUser.profile_pic}` : `http://localhost:8000/images/users/${props.currentUser.id}/${props.currentUser.profile_pic}`} className="user-pic" alt={`${props.currentUser.username}`} />
             {props.currentUser.username}
@@ -52,7 +40,19 @@ const LoggedInHead = props => {
         </li>
 
         <li className='nav-item'>
-          <Link to="logout" className="nav-link">
+          <Link to="" className="nav-link home">
+            <i className="fa fa-home" aria-hidden="true"></i> Home
+          </Link>
+        </li>
+
+        <li className='nav-item'>
+          <Link to="settings" className="nav-link settings">
+            <i className="fa fa-cog" aria-hidden="true"></i> Settings
+          </Link>
+        </li>
+
+        <li className='nav-item'>
+          <Link to="logout" className="nav-link logout">
              Log Out <i className="fa fa-sign-out" aria-hidden="true"></i>
           </Link>
         </li>
@@ -63,17 +63,53 @@ const LoggedInHead = props => {
 }
 
 class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hamburgerClick: false
+    }
+
+    this.menuClick = ev => {
+      let hamburger = document.getElementById("mobileNavbar");
+      hamburger.style.className = this.state.hamburgerClick ?
+        hamburger.setAttribute("class", "collapse navbar-collapse mobile-navbar") :
+        hamburger.setAttribute("class", "collapse navbar-collapse mobile-navbar active");
+      this.setState({hamburgerClick: !this.state.hamburgerClick});
+    }
+  }
+
   render() {
     return(
       <nav className="navbar navbar-light">
         <div className="container">
           <Link to="/" className="navbar-brand">
-            <span className="navbar-white"><img src="/distillery-maps-logo.png" className="head-logo" alt="Distillery Maps" /></span>
+            <span className="navbar-white">
+              <img src="/distillery-maps-logo.png" className="head-logo" alt="Distillery Maps" />
+            </span>
           </Link>
 
           <LoggedOutHead currentUser={this.props.currentUser} />
 
           <LoggedInHead currentUser={this.props.currentUser} />
+
+          <button
+            type="button"
+            id="toggle-btn"
+            className="navbar-toggle pull-right"
+            data-toggle="collapse"
+            data-target="#mobileNavbar"
+            onClick={this.menuClick.bind(this)}>
+            <i className="fa fa-bars" aria-hidden="true"></i>
+          </button>
+
+          <div className="collapse navbar-collapse mobile-navbar" id="mobileNavbar">
+            <div className="container">
+              <LoggedOutHead currentUser={this.props.currentUser} />
+              <LoggedInHead currentUser={this.props.currentUser} />
+            </div>
+          </div>
+
         </div>
       </nav>
     )
